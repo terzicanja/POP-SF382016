c7896fec61d77cceb5ecd8f6a122fbfa88976571
Anja Terzic <terzic.anja@gmail.com>
2017-10-27 21:55:37 +0200

    Radjen meni za namestaj

--- POP-SF382016/Program.cs
+++ POP-SF382016/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using POP_SF382016.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,154 @@ namespace POP_SF382016
 {
     class Program
     {
+        static List<Namestaj> namestaj { get; set;} = new List<Namestaj>();
+        static List<TipNamestaja> tipnamestaja { get; set;} = new List<TipNamestaja>();
+
+
         static void Main(string[] args)
         {
+            var s1 = new Salon()
+            {
+                Id = 1,
+                Naziv = "Forma FTNale",
+                Adresa = "Trg Dositeja Obradovica 6",
+                ZiroRacun = "840-00073666-83",
+                Email = "kontakt@ftn.uns.ac.rs",
+                MaticniBroj = 234324,
+                PIB = 323232,
+                Telefon = "021/342-343",
+                InternetAdresa = "hhtps://www.ftn.uns.ac.rs"
+            };
+
+            var tn1 = new TipNamestaja()
+            {
+                Id = 1,
+                Naziv = "Sofa"
+            };
+
+            var tn2 = new TipNamestaja()
+            {
+                Id = 2,
+                Naziv = "Regal",
+            };
+
+            var n1 = new Namestaj()
+            {
+                Id = 1,
+                Naziv = "Super sofa",
+                Sifra = "SF sifra za sofe",
+                Cena = 28,
+                TipNamestaja = tn1,
+                KolicinaUMagacinu = 2
+            };
+
+            namestaj.Add(n1);
+            Console.WriteLine($"====== Dobrodosli u salon {s1.Naziv}");
+            IspisGlavnogMenija();
+
         }
+
+        private static void IspisGlavnogMenija()
+        {
+            int izbor = 0;
+            do
+            {
+                Console.WriteLine("====Glavni meni====");
+                Console.WriteLine("1. Rad sa namestajem");
+                Console.WriteLine("2. Rad sa tipom namestaja");
+                Console.WriteLine("0. Izlaz iz aplikacije");
+
+                izbor = int.Parse(Console.ReadLine());
+            } while (izbor < 0 || izbor > 2);
+
+            switch (izbor)
+            {
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                case 1:
+                    IspisiMeniNamestaja();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void IspisiMeniNamestaja()
+        {
+            int izbor = 0;
+            do
+            {
+                Console.WriteLine("====Meni namestaja====");
+                Console.WriteLine("1. Izlistaj namestaj");
+                Console.WriteLine("2. Dodaj novi namestaj");
+                Console.WriteLine("3. Izmeni namestaj");
+                Console.WriteLine("4. Obrisi postojeci");
+                Console.WriteLine("0. Povratak u glavni meni");
+
+                izbor = int.Parse(Console.ReadLine());
+            } while (izbor<0 || izbor>4);
+
+            switch (izbor)
+            {
+                case 0:
+                    IspisGlavnogMenija();
+                    break;
+                case 1:
+                    IzlistajNamestaj();
+                    break;
+                case 2:
+                    DodavanjeNovogNamestaja();
+                    break;
+                case 3:
+                    IzmenaNamestaja();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void IzlistajNamestaj()
+        {
+            Console.WriteLine("===Izlistavanje namestaja===");
+            for (int i = 0; i < namestaj.Count; i++)
+            {
+                if (!namestaj[i].Obrisan)
+                {
+                    Console.WriteLine($"{i+1}.{namestaj[i].Naziv}, cena: {namestaj[i].Cena}, tip namestaja: {namestaj[i].TipNamestaja}");
+                }
+            }
+            IspisiMeniNamestaja();
+        }
+
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
